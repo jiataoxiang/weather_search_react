79d731c35b17ab63d01935474dc0324e10232aeb
jiatao <a389157511@gmail.com>
2020-06-19 15:57:29 -0400

    auto locate

--- weather_search/backend/server.js
+++ weather_search/backend/server.js
@@ -12,21 +12,33 @@ app.get('/test', (req, res) => {
     res.send("Hello World!");
 })
 
-// app.get('/location/:latlng', (req, res) => {
-//     latlng = this.params.latlng.split(",")
-//     console.log(latlng)
-//     let url = `http://api.map.baidu.com/geocoder/v2/callback=renderReverse&location=${latlng[0]},${latlng[1]}&output=json&pois=1&ak=zsAmLtsAxPFnfj5qdd976miZDbGEODzz`
-//     Axios.get(url).then((response) => {
-//         console.log(response);
-//         res.json(response.data);
-//     }).catch((error) => {
-//         console.log(error)
-//     })
-// });
+app.get('/autoweather/:city', (req, res) => {
+    let city = req.params.city;
+    console.log(city)
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=[your key]`
+    Axios.get(url).then((response) => {
+        console.log(response);
+        res.json(response.data);
+    }).catch((error) => {
+        console.log(error)
+    })
+})
+
+app.get('/location/:latlng', (req, res) => {
+    latlng = req.params.latlng.split(",")
+    console.log(latlng)
+    let url = `https://api.opencagedata.com/geocode/v1/json?q=${latlng[0]}+${latlng[1]}&key=[your key]`
+    Axios.get(url).then((response) => {
+        console.log(response);
+        res.json(response.data);
+    }).catch((error) => {
+        console.log(error)
+    })
+});
 
 app.get('/weather/:city', (req, res) => {
     console.log(req.params.city);
-    let url = `http://apis.juhe.cn/simpleWeather/query?city=${req.params.city}&key=3a254b2f20e2a6f217f3405cb3c0a876`
+    let url = `http://apis.juhe.cn/simpleWeather/query?city=${req.params.city}&key=[your key]`
     let encoded = encodeURI(url)
     Axios.get(encoded).then((response) => {
         console.log(response);
--- weather_search/src/component/location.search.js
+++ weather_search/src/component/location.search.js
@@ -1,64 +1,8 @@
 import React, {Component} from 'react';
 import Axios from 'axios';
 
-//计算字符串长度
-String.prototype.strLen = function() {
-    var len = 0;
-    for (var i = 0; i < this.length; i++) {
-        if (this.charCodeAt(i) > 255 || this.charCodeAt(i) < 0) len += 2; else len ++;
-    }
-    return len;
-}
-//将字符串拆成字符，并存到数组中
-String.prototype.strToChars = function(){
-   var chars = new Array();
-   for (var i = 0; i < this.length; i++){
-       chars[i] = [this.substr(i, 1), this.isCHS(i)];
-   }
-   String.prototype.charsArray = chars;
-   return chars;
-}
-//判断某个字符是否是汉字
-String.prototype.isCHS = function(i){
-   if (this.charCodeAt(i) > 255 || this.charCodeAt(i) < 0) 
-       return true;
-   else
-       return false;
-}
-//截取字符串（从start字节到end字节）
-String.prototype.subCHString = function(start, end){
-   var len = 0;
-   var str = "";
-   this.strToChars();
-   for (var i = 0; i < this.length; i++) {
-       if(this.charsArray[i][1])
-           len += 2;
-       else
-           len++;
-       if (end < len)
-           return str;
-       else if (start < len)
-           str += this.charsArray[i][0];
-   }
-   return str;
-}
-//截取字符串（从start字节截取length个字节）
-String.prototype.subCHStr = function(start, length){
-   return this.subCHString(start, start + length);
-}
 
 export default class WeatherSearch extends Component {
-    MP(ak) {
-        return new Promise(function(resolve, reject) {
-            var script = document.createElement('script')
-            script.type = 'text/javascript'
-            script.src = `http://api.map.baidu.com/api?v=2.0&ak=${ak}&callback=init`;
-            document.head.appendChild(script)
-            window.init = () => {
-                resolve(window.BMap)
-            }
-        })
-    }
 
     constructor(props) {
         super(props);
@@ -75,38 +19,30 @@ export default class WeatherSearch extends Component {
     }
     
     componentDidMount() {
-        this.MP("PAZGg1jfimrTHCIAsoQc9zfsRbh").then(BMap=>{
-            var map = new BMap.Map('allmap');            // 创建Map实例
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition((position)=>{
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((position)=>{
                 let latitude = position.coords.latitude
                 let longitude = position.coords.longitude
-                var point = new BMap.Point(latitude, longitude); // 创建点坐标
-                var gc = new BMap.Geocoder();
-                gc.getLocation(point, (rs) => {
-                    var addComp = rs.addressComponents;
-                    let city = addComp.city.subCHStr(0, addComp.city.strLen() - 2)
-                    let url = `weather/${city}`
-                    Axios.get(url).then((response)=>{
-                        console.log(response)
-                        let w = response.data.result.realtime;
+                let url = `location/${latitude},${longitude}`
+                Axios.get(url).then((response) => {
+                    let city = response.data.results[0].components.city
+                    let inner_url = `autoweather/${city}`
+                    Axios.get(inner_url).then((res) => {
+                        let w = res.data;
                         this.setState({
                             city: city,
-                            weather: w.info,
-                            tmp: w.temperature,
-                            direct: w.direct,
-                            humidity: w.humidity,
+                            weather: w.weather[0].main,
+                            tmp: (parseFloat(w.main.temp) - 273.15).toFixed(0),
+                            direct: w.wind.deg,
+                            humidity: w.main.humidity,
                         })
-                    }).catch((err) => {
-                        alert("这个城市不存在,请重新输入")
                     })
+
                 })
-            })
-            } else {
-                alert("你的浏览器不支持Geolocation！")
-            }
-        });
+            });
+        }  
     }
+
     render(){
         return(
             <div className="LocationSearch">
